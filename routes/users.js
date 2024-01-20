0758104c9572c27cdbe1a18f21951b16f1a0c0fe
Rashin <rashinkp001@gmail.com>
2024-01-20 18:58:56 +0530

    req for admission image uploaded successfully

--- routes/users.js
+++ routes/users.js
@@ -26,13 +26,35 @@ router.get('/login',(req,res)=>{
 router.get('/admission-req',verifyLoginUser,(req,res)=>{
   res.render('principal/add-student',{user:true})
 })
-router.post('/req-admission', verifyLoginUser, (req, res) => {
-  req.body.userId = req.session.user._id;
-  reqHelpers.insertRequestAdmission(req.body).then((response) => {
-      console.log(response);
-      res.redirect('/');
-  });
+router.post('/req-admission', verifyLoginUser, async (req, res) => {
+  try {
+    if (req.file) {
+      let image = req.file;
+      const studentData = req.body;
+      const result = await reqHelpers.insertRequestAdmission(studentData);
+
+      const id = result.insertedId; // Get the inserted document's ID
+
+      const destinationPath = path.join(__dirname, '../public/images/student', id + '.jpg');
+      
+      fs.rename(image.path, destinationPath, (err) => {
+        if (!err) {
+          res.redirect('/');
+        } else {
+          console.log(err);
+          res.status(500).send('Error moving the file');
+        }
+      });
+    } else {
+      console.log('No file uploaded');
+      res.status(400).send('No file uploaded');
+    }
+  } catch (error) {
+    console.error('Error in /add-student route:', error);
+    res.status(500).send('Internal Server Error');
+  }
 });
+
 
 router.get('/user-login',(req,res)=>{
   res.render('user/user-login')
